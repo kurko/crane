ab32bac2ec5c4db6409cd028926761785eb33e55
Alexandre de Oliveira <chavedomundo@gmail.com>
2010-05-25 00:05:08 -0300

    If set, getting only files from either today or yesterday through 'push today' or 'push yesterday'.

--- lib/shell/shell.rb
+++ lib/shell/shell.rb
@@ -10,6 +10,7 @@ module Shell
       
       @options = Shell::Parser.get_options @argv
       @command = Shell::Parser.get_command @argv
+      @arguments = Shell::Parser.get_arguments @argv
     end
     
   end
@@ -19,6 +20,32 @@ module Shell
     def self.text
       STDOUT.flush
       STDIN.gets.strip
+    end
+    
+    def self.yesno message
+      STDOUT.flush
+      has_result = false
+      while has_result == false
+        print message.strip + " "
+        input = STDIN.gets.strip
+        
+        if input == "yes" or input == "y" then
+          final_input = "yes"
+        elsif input == "no" or input == "n" then
+          final_input = "no"
+        end
+        
+        if ( final_input == "yes" or final_input == "no" )
+          has_result = true
+        end
+      end
+      
+      if final_input == "yes"
+        result = true
+      else
+        result = false
+      end
+      result
     end
     
   end
@@ -60,6 +87,27 @@ module Shell
         end
       }
       @options
+    end # get_options
+    
+    # get arguments. arguments are anything written besides the command and options.
+    # in 'push today --list', 'today' is the argument
+    def self.get_arguments argv
+      @arguments = Array.new
+      i = 0
+      argv.each {
+        |e|
+        
+        i+= 1
+        next if i == 1
+        
+        e_length = e.length
+        if e[0,2] != "--" and e[0,1] != "-"
+          
+          @arguments.push e[0,e_length]
+        end
+        
+      }
+      @arguments
     end # get_options
     
     def self.is_option option, argv = Array.new
--- lib/uplift/commands/push.rb
+++ lib/uplift/commands/push.rb
@@ -10,13 +10,59 @@ module Uplift::Commands
         puts "No files were found."
         exit
       elsif @local_files.length == 1 then
-        puts "Push 1 file to the server?"
+        do_push_files = Shell::Input.yesno "Push 1 file to the server? [yes or no]"
       else
-        puts "Push "+@local_files.length.to_s+" files to the server?"
+        do_push_files = Shell::Input.yesno "Push "+@local_files.length.to_s+" files to the server? [yes or no]"
       end
+      
+      unless do_push_files
+        exit
+      end
+      
+      puts "Send"
+      
       
 
     end # run
+    
+    # if user defines a time interval for the file, check if it complies
+    def within_defined_interval? file
+      result = false
+      time = Time.new
+      
+      #puts Time.local(2000, 2, -1)
+      
+      #today's files
+      if
+        @arguments.include? "today" then
+        
+        file_time = Time.at File.stat(file).ctime
+        if( file_time.year == time.year and
+            file_time.month == time.month and
+            file_time.day == time.day )
+        then
+          result = true
+        end
+        
+      elsif
+        @arguments.include? "yesterday" then
+        
+        yesterday = time - (60 * 60 * 24)
+        file_time = Time.at File.stat(file).ctime
+        if( file_time.year == yesterday.year and
+            file_time.month == yesterday.month and
+            file_time.day == yesterday.day )
+        then
+          result = true
+        end
+      
+      else
+        result = true
+      end
+
+      result
+    end
+    
     
     # defines what files are to be sent
     def get_files search_folder = ""
@@ -39,15 +85,18 @@ module Uplift::Commands
         if File.stat(file).directory? then
           local_files += get_files(file+"/").flatten
         else
-          if is_option "list" then
-            puts file
+          
+          if within_defined_interval? file then
+          
+            if is_option "list" then
+              puts file
+            end
+          
+            local_files.push file
           end
-          local_files.push file
         end
       end
-      
       local_files
-      
     end #get_files
     
     def help
@@ -55,4 +104,5 @@ module Uplift::Commands
     end
   
   end
+  
 end
